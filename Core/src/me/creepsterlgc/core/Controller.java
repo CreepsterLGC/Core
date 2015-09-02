6656c75dc4443ada52841754cc0f33f545485569
CreepsterLGC <max.nitschke@outlook.de>
2015-09-02 14:48:35 +0200

    Cleaning

--- Core/src/me/creepsterlgc/core/Controller.java
+++ Core/src/me/creepsterlgc/core/Controller.java
@@ -8,7 +8,6 @@ import org.spongepowered.api.Game;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.text.Text;
-import com.flowpowered.math.vector.Vector3d;
 
 
 public class Controller {
@@ -21,13 +20,5 @@ public class Controller {
 	public static void broadcast(Text text) { SERVER.broadcast(text); }
 	
 	public static Collection<Player> getPlayers() { return Core.getInstance().getGame().getServer().getOnlinePlayers(); }
-	
-	public static boolean teleport(Player player, Player target) { return player.transferToWorld(target.getWorld().getName(), new Vector3d(target.getLocation().getX(), target.getLocation().getY(), target.getLocation().getZ())); }
-	
-	public static void kick(Player player, Text reason) { player.kick(reason); }
-	
-//	public static void ban(PLAYER player, CommandSource sender, String reason, double duration) { BAN ban = new BAN(player.getUUID(), sender.getName().toLowerCase(), reason, System.currentTimeMillis(), duration); ban.insert(); }
-//	public static void unban(String uuid) { if(STORAGE.getBan(uuid) == null) return; BAN ban = STORAGE.getBan(uuid); ban.delete(); }
-//	public static BAN checkban(String uuid) { return STORAGE.getBan(uuid) != null ? STORAGE.getBan(uuid) : null; }
 	
 }
