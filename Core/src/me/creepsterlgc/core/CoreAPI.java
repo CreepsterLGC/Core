397cf8912fd69eaaed7c95eeca240382653e859f
CreepsterLGC <max.nitschke@outlook.de>
2015-09-19 19:32:37 +0200

    some commenting

--- Core/src/me/creepsterlgc/core/CoreAPI.java
+++ Core/src/me/creepsterlgc/core/CoreAPI.java
@@ -23,7 +23,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the ban could be issued, false if not
+	 * RETURNS: true if the ban could be issued, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * sender = Sender issuing the ban
@@ -42,7 +42,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the ban has been removed, false if not
+	 * RETURNS: true if the ban has been removed, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * 
@@ -56,7 +56,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the uuid is banned, false if not
+	 * RETURNS: true if the uuid is banned, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * 
@@ -71,7 +71,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the ban mute be issued, false if not
+	 * RETURNS: true if the ban mute be issued, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * reason = The ban's reason
@@ -88,7 +88,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the ban mute has been issued, false if not
+	 * RETURNS: true if the ban mute has been issued, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * 
@@ -102,7 +102,7 @@ public class CoreAPI {
 	}
 	
 	/*
-	 * RETURNS: true if the uuid is muted, false if not
+	 * RETURNS: true if the uuid is muted, false if not.
 	 * 
 	 * uuid = The target's uuid
 	 * 
@@ -117,11 +117,33 @@ public class CoreAPI {
 		return player.transferToWorld(w.getWorld(), new Vector3d(w.getX(), w.getY(), w.getZ()));
 	}
 	
+	/*
+	 * RETURNS: true if the player could be teleported, false if not.
+	 * 
+	 * player = The target player
+	 * warp = Name of the warp
+	 * 
+	 * EXAMPLE: (player, "beach")
+	 * Would teleport player to warp "beach".
+	 * 
+	 */
+	
 	public boolean spawn(Player player, String spawn) {
 		SPAWN s = DATABASE.getSpawn(spawn.toLowerCase());
 		if(s == null) return false;
 		return player.transferToWorld(s.getWorld(), new Vector3d(s.getX(), s.getY(), s.getZ()));
 	}
+	
+	/*
+	 * RETURNS: true if the player could be teleported, false if not.
+	 * 
+	 * player = The target player
+	 * spawn = Name of the spawn
+	 * 
+	 * EXAMPLE: (player, "market")
+	 * Would teleport player to spawn "market".
+	 * 
+	 */
 	
 	public boolean setNick(String uuid, String nick) {
 		PLAYER player = DATABASE.getPlayer(uuid);
@@ -131,33 +153,104 @@ public class CoreAPI {
 		return true;
 	}
 	
+	/*
+	 * RETURNS: true if the nick could be set, false if not.
+	 * 
+	 * uuid = The target's uuid
+	 * nick = The nickname. Supports color codes (ex: "&cCreep&6ster&7LGC" would be possible)
+	 * 
+	 * EXAMPLE: ("my-unique-id", "&6Jim")
+	 * Would set the nick for "my-unique-id" to "&6Jim".
+	 * 
+	 */
+	
 	public String getNick(String uuid) {
 		PLAYER player = DATABASE.getPlayer(uuid);
 		if(player == null) return ""; return player.getNick();
 	}
+	
+	/*
+	 * RETURNS: The nick of the uuid. If the uuid could not be found it returns "".
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would return the nick of "my-unique-id".
+	 * 
+	 */
 	
 	public boolean addMoney(String uuid, double amount) {
 		PLAYER player = DATABASE.getPlayer(uuid); if(player == null) return false;
 		player.addMoney(amount); player.update(); return true;
 	}
 	
+	/*
+	 * RETURNS: true if the money could be added, false if not.
+	 * 
+	 * uuid = The target's uuid
+	 * amount = The amount to add to the players balance
+	 * 
+	 * EXAMPLE: ("my-unique-id", 2500)
+	 * Would add $2500 to "my-unique-id".
+	 * 
+	 */
+	
 	public boolean removeMoney(String uuid, double amount) {
 		PLAYER player = DATABASE.getPlayer(uuid); if(player == null) return false;
 		player.removeMoney(amount); player.update(); return true;
 	}
+	
+	/*
+	 * RETURNS: true if the money could be removed, false if not.
+	 * 
+	 * uuid = The target's uuid
+	 * amount = The amount to remove from the players balance
+	 * 
+	 * EXAMPLE: ("my-unique-id", 2500)
+	 * Would remove $2500 to "my-unique-id".
+	 * 
+	 */
 	
 	public boolean setMoney(String uuid, double amount) {
 		PLAYER player = DATABASE.getPlayer(uuid); if(player == null) return false;
 		player.setMoney(amount); player.update(); return true;
 	}
 	
+	/*
+	 * RETURNS: true if the money could be set, false if not.
+	 * 
+	 * uuid = The target's uuid
+	 * amount = The amount to set the players balance to
+	 * 
+	 * EXAMPLE: ("my-unique-id", 300)
+	 * Would set the balance of "my-unique-id" to $300.
+	 * 
+	 */
+	
 	public double getMoney(String uuid) {
 		PLAYER player = DATABASE.getPlayer(uuid);
 		if(player == null) return 0; return player.getMoney();
 	}
 	
+	/*
+	 * RETURNS: The players balance. If the player could not be found it returns 0.
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would return the money of "my-unique-id".
+	 * 
+	 */
+	
 	public void broadcast(Text message) {
 		Controller.broadcast(Texts.of(TextColors.GOLD, "Server", TextColors.GRAY, ": ", TextColors.WHITE, message));
 	}
+	
+	/*
+	 * RETURNS: Nothing
+	 * 
+	 * message = The message to be broadcasted
+	 * 
+	 */
 	
 }
