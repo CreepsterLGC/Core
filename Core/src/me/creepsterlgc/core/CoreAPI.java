a43b8afbb20e0aafc06152649a18427210d31777
CreepsterLGC <max.nitschke@outlook.de>
2015-09-19 19:07:09 +0200

    commenting

--- Core/src/me/creepsterlgc/core/CoreAPI.java
+++ Core/src/me/creepsterlgc/core/CoreAPI.java
@@ -17,6 +17,11 @@ import com.flowpowered.math.vector.Vector3d;
 
 public class CoreAPI {
 	
+	public boolean addBan(String uuid, String sender, String reason, double time, double duration) {
+		if(DATABASE.getBan(uuid) != null) return false;
+		BAN ban = new BAN(uuid, sender.toLowerCase(), reason, time, duration); ban.insert(); return true;
+	}
+	
 	/*
 	 * RETURNS: true if the ban could be issued, false if not
 	 * 
@@ -31,9 +36,9 @@ public class CoreAPI {
 	 * 
 	 */
 	
-	public boolean addBan(String uuid, String sender, String reason, double time, double duration) {
-		if(DATABASE.getBan(uuid) != null) return false;
-		BAN ban = new BAN(uuid, sender.toLowerCase(), reason, time, duration); ban.insert(); return true;
+	public boolean removeBan(String uuid) {
+		if(DATABASE.getBan(uuid) == null) return false;
+		BAN ban = DATABASE.getBan(uuid); ban.delete(); return true;
 	}
 	
 	/*
@@ -46,9 +51,8 @@ public class CoreAPI {
 	 * 
 	 */
 	
-	public boolean removeBan(String uuid) {
-		if(DATABASE.getBan(uuid) == null) return false;
-		BAN ban = DATABASE.getBan(uuid); ban.delete(); return true;
+	public boolean isBanned(String uuid) {
+		return DATABASE.getBan(uuid) != null;
 	}
 	
 	/*
@@ -61,23 +65,51 @@ public class CoreAPI {
 	 * 
 	 */
 	
-	public boolean isBanned(String uuid) {
-		return DATABASE.getBan(uuid) != null;
-	}
-	
 	public boolean addMute(String uuid, double duration, String reason) {
 		if(DATABASE.getMute(uuid) != null) return false;
 		MUTE mute = new MUTE(uuid, duration, reason); mute.insert(); return true;
 	}
+	
+	/*
+	 * RETURNS: true if the ban mute be issued, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * reason = The ban's reason
+	 * duration = You go with: (System.currentTimeMillis() + TIME_IN_MILLISECONDS)
+	 * 
+	 * EXAMPLE: ("my-unique-id", "Don't spam please!", System.currentTimeMillis() + 1000 * 60)
+	 * Would mute "my-unique-id" for 1 minute.
+	 * 
+	 */
 	
 	public boolean removeMute(String uuid) {
 		if(DATABASE.getMute(uuid) == null) return false;
 		MUTE mute = DATABASE.getMute(uuid); mute.delete(); return true;
 	}
 	
+	/*
+	 * RETURNS: true if the ban mute has been issued, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would unmute "my-unique-id".
+	 * 
+	 */
+	
 	public boolean isMuted(String uuid) {
 		return DATABASE.getMute(uuid) != null;
 	}
+	
+	/*
+	 * RETURNS: true if the uuid is muted, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would check "my-unique-id".
+	 * 
+	 */
 	
 	public boolean warp(Player player, String warp) {
 		WARP w = DATABASE.getWarp(warp.toLowerCase());
