f6e3228a281cc8b39e93f33c1a9da0186f837023
CreepsterLGC <max.nitschke@outlook.de>
2015-09-19 19:02:13 +0200

    testing

--- Core/src/me/creepsterlgc/core/CoreAPI.java
+++ Core/src/me/creepsterlgc/core/CoreAPI.java
@@ -17,15 +17,49 @@ import com.flowpowered.math.vector.Vector3d;
 
 public class CoreAPI {
 	
+	/*
+	 * RETURNS: true if the ban could be issued, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * sender = Sender issuing the ban
+	 * reason = The ban's reason
+	 * time = Systemtime when the ban has been issued
+	 * duration = If the ban is permanent you go with 0. Otherwise you do: (System.currentTimeMillis() + TIME_IN_MILLISECONDS)
+	 * 
+	 * EXAMPLE: ("my-unique-id", "creepsterlgc", "griefing my house", System.currentTimeMillis(), System.currentTimeMillis() + 1000 * 3600)
+	 * Would ban "my-unique-id" for 1 hour.
+	 * 
+	 */
+	
 	public boolean addBan(String uuid, String sender, String reason, double time, double duration) {
 		if(DATABASE.getBan(uuid) != null) return false;
 		BAN ban = new BAN(uuid, sender.toLowerCase(), reason, time, duration); ban.insert(); return true;
 	}
 	
+	/*
+	 * RETURNS: true if the ban has been removed, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would unban "my-unique-id".
+	 * 
+	 */
+	
 	public boolean removeBan(String uuid) {
 		if(DATABASE.getBan(uuid) == null) return false;
 		BAN ban = DATABASE.getBan(uuid); ban.delete(); return true;
 	}
+	
+	/*
+	 * RETURNS: true if the uuid is banned, false if not
+	 * 
+	 * uuid = The target's uuid
+	 * 
+	 * EXAMPLE: ("my-unique-id")
+	 * Would check "my-unique-id".
+	 * 
+	 */
 	
 	public boolean isBanned(String uuid) {
 		return DATABASE.getBan(uuid) != null;
