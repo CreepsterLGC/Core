adbfff5682ad439d47f4c71b7d90a3965e4e4100
CreepsterLGC <max.nitschke@outlook.de>
2015-11-15 14:46:02 +0100

    Core is now compatible with SpongeForge 814+

--- src/main/java/me/creepsterlgc/core/utils/ItemUtils.java
+++ src/main/java/me/creepsterlgc/core/utils/ItemUtils.java
@@ -13,14 +13,13 @@ import org.spongepowered.api.item.Enchantment;
 import org.spongepowered.api.item.Enchantments;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.ItemStackBuilder;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 public class ItemUtils {
 	
 	public static ItemStack build(ItemType type, int quantity) {
-		return Controller.getGame().getRegistry().createBuilder(ItemStackBuilder.class).itemType(type).quantity(quantity).build();
+		return Controller.getGame().getRegistry().createBuilder(ItemStack.Builder.class).itemType(type).quantity(quantity).build();
 	}
 	
 	public static void drop(ItemStack item, Player player) {
